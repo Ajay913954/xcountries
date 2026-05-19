408285a0369c07fef065a3003be5fbf89f53ecdb
Ajay Gupta <ajayg913654@gmail.com>
2026-05-19 20:19:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://xcountries-backend.labs.crio.do/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {countries.map((country) => (
+        <div className="card" key={country.name}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            className="flag"
+          />
+          <p>{country.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
