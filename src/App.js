f606cbed0a53fa11135f96689d6144ae90e9d520
Ajay Gupta <ajayg913654@gmail.com>
2026-05-26 23:10:31 +0530

    Added countries search project

--- src/App.js
+++ src/App.js
@@ -2,32 +2,95 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [countries, setCountries] = useState([]);
 
+  // Store all countries
+  const [countries, setCountries] =
+    useState([]);
+
+  // Search input
+  const [search, setSearch] =
+    useState("");
+
+  // Fetch API
   useEffect(() => {
-    fetch("https://xcountries-backend.labs.crio.do/all")
-      .then((response) => response.json())
+
+    fetch(
+      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+    )
+      .then((response) =>
+        response.json()
+      )
       .then((data) => {
+
         setCountries(data);
+
       })
       .catch((error) => {
-        console.error("Error fetching data: ", error);
+
+        console.error(error);
+
       });
+
   }, []);
 
+  // Filter countries
+  const filteredCountries =
+    countries.filter((country) =>
+
+      country.common
+        .toLowerCase()
+        .includes(
+          search.toLowerCase()
+        )
+
+    );
+
   return (
-    <div className="container">
-      {countries.map((country) => (
-        <div className="card" key={country.name}>
-          <img
-            src={country.flag}
-            alt={`Flag of ${country.name}`}
-            className="flag"
-          />
-          <p>{country.name}</p>
-        </div>
-      ))}
+
+    <div className="app">
+
+      {/* Search Input */}
+      <input
+        type="text"
+        placeholder="Search for countries..."
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+        className="search-input"
+      />
+
+      {/* Countries Grid */}
+      <div className="countries-container">
+
+        {filteredCountries.map(
+          (country) => (
+
+            <div
+              className="countryCard"
+              key={country.common}
+            >
+
+              {/* Flag */}
+              <img
+                src={country.png}
+                alt={country.common}
+              />
+
+              {/* Country Name */}
+              <p>
+                {country.common}
+              </p>
+
+            </div>
+
+          )
+        )}
+
+      </div>
+
     </div>
+
   );
 }
 
